a2b4241c48805013f8f2892d313204428221c99d
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-08-29 23:55:04 +0800

    0.2.17

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,12 +18,12 @@
                     </ion-card-header>
                 </ion-card>
 
-                <!-- <ion-card>
+                <ion-card>
                     <ion-card-header>
                         <ion-card-title>FIRST AUTO UPDATE</ion-card-title>
                         <ion-card-subtitle>Test for auto update feature</ion-card-subtitle>
                     </ion-card-header>
-                </ion-card> -->
+                </ion-card>
             </div>
         </ion-content>
         <Help></Help>
