1d811c869c6028f696e9ddc0610ba9bd72941637
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-09-05 05:47:05 +0800

    update

--- src/views/internalFlow/laminar.vue
+++ src/views/internalFlow/laminar.vue
@@ -18,35 +18,51 @@
                 <ion-list>
                     <ion-item>
                         <ion-label>P : </ion-label>
-                        <ion-input v-model="pdrop" type="number" placeholder="Enter Pressure Drop ( kPa )"></ion-input>
+                        <ion-input v-if="!P2Show && !P1Show" v-model="pdrop" type="number" placeholder="Enter Pressure Drop ( kPa )"></ion-input>
+                        <ion-input v-if="P2Show" :value="P2Show" readonly></ion-input>
+                        <ion-input v-if="P1Show" :value="P1Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>&rho; : </ion-label>
-                        <ion-input v-model="rho" type="number" placeholder="Enter Fluid Density ( kg/m^3 )"></ion-input>
+                        <ion-input v-if="!rho1Show && !rho2Show" v-model="rho" type="number" placeholder="Enter Fluid Density ( kg/m^3 )"></ion-input>
+                        <ion-input v-if="rho1Show" :value="rho1Show" readonly></ion-input>
+                        <ion-input v-if="rho2Show" :value="rho2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>L : </ion-label>
-                        <ion-input v-model="l" type="number" placeholder="Enter Length ( m )"></ion-input>
+                        <ion-input v-if="!l1Show && !l2Show" v-model="l" type="number" placeholder="Enter Length ( m )"></ion-input>
+                        <ion-input v-if="l1Show" :value="l1Show" readonly></ion-input>
+                        <ion-input v-if="l2Show" :value="l2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>&theta; : </ion-label>
-                        <ion-input v-model="theta" type="number" placeholder="Enter Tilt Angle ( degrees )"></ion-input>
+                        <ion-input v-if="!theta1Show && !theta2Show" v-model="theta" type="number" placeholder="Enter Tilt Angle ( degrees )"></ion-input>
+                        <ion-input v-if="theta1Show" :value="theta1Show" readonly></ion-input>
+                        <ion-input v-if="theta2Show" :value="theta2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>D : </ion-label>
-                        <ion-input v-model="d" type="number" placeholder="Enter Diameter ( degrees )"></ion-input>
+                        <ion-input v-if="!d1Show && !d2Show" v-model="d" type="number" placeholder="Enter Diameter ( m )"></ion-input>
+                        <ion-input v-if="d1Show" :value="d1Show" readonly></ion-input>
+                        <ion-input v-if="d2Show" :value="d2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>&mu; : </ion-label>
-                        <ion-input v-model="mu" type="number" placeholder="Enter Dynamic Viscosity ( kg/ms )"></ion-input>
+                        <ion-input v-if="!mu1Show && !mu2Show" v-model="mu" type="number" placeholder="Enter Dynamic Viscosity ( kg/ms )"></ion-input>
+                        <ion-input v-if="mu1Show" :value="mu1Show" readonly></ion-input>
+                        <ion-input v-if="mu2Show" :value="mu2Show" readonly></ion-input>
                     </ion-item>
                 </ion-list>
                 <br>
                 <ion-item>
                     <ion-label v-if="selection == 'vave'">V<sub>avg</sub> : </ion-label>
                     <ion-label v-if="selection == 'flowRate'">Flow Rate : </ion-label>
-                    <ion-input v-if="selection == 'vave'" :value="vaveShow" readonly></ion-input>
-                    <ion-input v-if="selection == 'flowRate'" :value="FRShow" readonly></ion-input>
+
+                    <ion-input v-model="va" v-if="selection == 'vave' && !vaveShow" type="number" placeholder="Enter Average Velocity ( m/s )"></ion-input>
+                    <ion-input v-model="fr" v-if="selection == 'flowRate' && !FRShow" type="number" placeholder="Enter Volume Flow Rate ( m^3/s )"></ion-input>
+
+                    <ion-input v-if="selection == 'vave' && vaveShow" :value="vaveShow" readonly></ion-input>
+                    <ion-input v-if="selection == 'flowRate' && FRShow" :value="FRShow" readonly></ion-input>
                 </ion-item>
                 <br>
                 <ion-button color="dark" @click="clear()" id="clear">Clear</ion-button>
@@ -100,7 +116,9 @@ export default defineComponent({
             l: "",
             theta: "",
             d: "",
-            mu: ""
+            mu: "",
+            fr: "",
+            va: ""
         }
     },
     computed: {
@@ -117,16 +135,102 @@ export default defineComponent({
             } else {
                 return ""
             }
-        }
+        },
+        P1Show() {
+            if (this.fr != "" && this.rho != "" && this.l != "" && this.theta != "" && this.d != "" && this.mu != "" && this.selection == "flowRate") {
+                return parseFloat(0.001*((this.fr*128*this.mu*this.l/(Math.PI*this.d**4))+(this.rho*9.81*this.l*Math.sin(this.theta*Math.PI/180)))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        P2Show() {
+            if (this.rho != "" && this.l != "" && this.theta != "" && this.d != "" && this.mu != "" && this.selection == "vave" && this.va != "") {
+                return parseFloat(0.001*((this.va*32*this.mu*this.l/this.d**2)+(this.rho*9.81*this.l*Math.sin(this.theta*Math.PI/180)))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        rho1Show() {
+            if (this.pdrop != "" && this.l != "" && this.theta != "" && this.d != "" && this.mu != "" && this.selection == "flowRate" && this.fr != "") {
+                return parseFloat((1000*this.pdrop-(this.fr*128*this.mu*this.l/(Math.PI*this.d**4)))/(9.81*this.l*Math.sin(this.theta*Math.PI/180))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        rho2Show() {
+            if (this.pdrop != "" && this.l != "" && this.theta != "" && this.d != "" && this.mu != "" && this.selection == "vave" && this.va != "") {
+                return parseFloat((1000*this.pdrop-(this.va*32*this.mu*this.l/(this.d**2)))/(9.81*this.l*Math.sin(this.theta*Math.PI/180))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        l1Show() {
+            if (this.pdrop != "" && this.rho != "" && this.theta != "" && this.d != "" && this.mu != "" && this.selection == "vave" && this.va != "") {
+                return parseFloat((1000*this.pdrop)/(((this.va*32*this.mu)/(this.d**2))+(this.rho*9.81*Math.sin(this.theta*Math.PI/180)))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        l2Show() {
+            if (this.pdrop != "" && this.rho != "" && this.theta != "" && this.d != "" && this.mu != "" && this.selection == "flowRate" && this.fr != "") {
+                return parseFloat(1000*this.pdrop/(((this.fr*128*this.mu)/(Math.PI*this.d**4))+(this.rho*9.81*Math.sin(this.theta*Math.PI/180)))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        theta1Show() {
+            if (this.pdrop != "" && this.rho != "" && this.l != "" && this.d != "" && this.mu != "" && this.selection == "flowRate" && this.fr != "") {
+                return parseFloat(180*Math.asin((1000*this.pdrop-(this.fr*128*this.mu*this.l/(Math.PI*this.d**4)))/(this.rho*9.81*this.l))/Math.PI).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        theta2Show() {
+            if (this.pdrop != "" && this.rho != "" && this.l != "" && this.d != "" && this.mu != "" && this.selection == "vave" && this.va != "") {
+                return parseFloat(180*Math.asin((1000*this.pdrop-(this.va*32*this.mu*this.l/(this.d**2)))/(this.rho*9.81*this.l))/Math.PI).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        d1Show() {
+            if (this.pdrop != "" && this.rho != "" && this.l != "" && this.theta != "" && this.mu != "" && this.selection == "vave" && this.va != "") {
+                return parseFloat(Math.sqrt((this.va*32*this.mu*this.l)/(1000*this.pdrop-this.rho*9.81*this.l*Math.sin(this.theta*Math.PI/180)))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        d2Show() {
+            if (this.pdrop != "" && this.rho != "" && this.l != "" && this.theta != "" && this.mu != "" && this.selection == "flowRate" && this.fr != "") {
+                return parseFloat(Math.sqrt(Math.sqrt((this.fr*128*this.mu*this.l)/(Math.PI*(1000*this.pdrop-this.rho*9.81*this.l*Math.sin(this.theta*Math.PI/180)))))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        mu1Show() {
+            if (this.pdrop != "" && this.rho != "" && this.l != "" && this.theta != "" && this.d != "" && this.selection == "flowRate" && this.fr != "") {
+                return parseFloat((this.d**4*Math.PI*(1000*this.pdrop-this.rho*9.81*this.l*Math.sin(this.theta*Math.PI/180)))/(this.fr*128*this.l)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        mu2Show() {
+            if (this.pdrop != "" && this.rho != "" && this.l != "" && this.theta != "" && this.d != "" && this.selection == "vave" && this.va != "") {
+                return parseFloat((this.d**2*(1000*this.pdrop-this.rho*9.81*this.l*Math.sin(this.theta*Math.PI/180)))/(this.va*32*this.l)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
     },
     methods: {
         clear() {
-            this.pdrop = "",
-            this.rho = "",
-            this.l = "",
-            this.theta = "",
-            this.d = "",
+            this.pdrop = ""
+            this.rho = ""
+            this.l = ""
+            this.theta = ""
+            this.d = ""
             this.mu = ""
+            this.va = ""
+            this.fr = ""
         }
     },
     watch: {
--- src/views/internalFlow/loss.vue
+++ src/views/internalFlow/loss.vue
@@ -17,29 +17,42 @@
                     </ion-item>
                     <ion-item>
                         <ion-label>V<sub>avg</sub> : </ion-label>
-                        <ion-input v-model="vave" type="number" placeholder="Enter Average Velocity ( m/s )"></ion-input>
+                        <ion-input v-if="!v1Show && !v2Show" v-model="vave" type="number" placeholder="Enter Average Velocity ( m/s )"></ion-input>
+                        <ion-input v-if="v1Show" :value="v1Show" readonly></ion-input>
+                        <ion-input v-if="v2Show" :value="v2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>&nbsp;f&nbsp;&nbsp;&nbsp; : </ion-label>
-                        <ion-input v-model="f" type="number" placeholder="Enter Friction Factor"></ion-input>
+                        <ion-input v-if="!f1Show && !f2Show" v-model="f" type="number" placeholder="Enter Friction Factor"></ion-input>
+                        <ion-input v-if="f1Show" :value="f1Show" readonly></ion-input>
+                        <ion-input v-if="f2Show" :value="f2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>&nbsp;L&nbsp;&nbsp;&nbsp; : </ion-label>
-                        <ion-input v-model="l" type="number" placeholder="Enter Length ( m )"></ion-input>
+                        <ion-input v-if="!l1Show && !l2Show" v-model="l" type="number" placeholder="Enter Length ( m )"></ion-input>
+                        <ion-input v-if="l1Show" :value="l1Show" readonly></ion-input>
+                        <ion-input v-if="l2Show" :value="l2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item>
                         <ion-label>&nbsp;D&nbsp;&nbsp;&nbsp; : </ion-label>
-                        <ion-input v-model="d" type="number" placeholder="Enter Hydraulic Diameter ( m )"></ion-input>
+                        <ion-input v-if="!d1Show && !d2Show" v-model="d" type="number" placeholder="Enter Hydraulic Diameter ( m )"></ion-input>
+                        <ion-input v-if="d1Show" :value="d1Show" readonly></ion-input>
+                        <ion-input v-if="d2Show" :value="d2Show" readonly></ion-input>
                     </ion-item>
                     <ion-item v-if="selection == 'p'">
                         <ion-label>&nbsp;&rho;&nbsp;&nbsp;&nbsp; : </ion-label>
-                        <ion-input v-model="rho" type="number" placeholder="Enter Fluid Density ( kg/m^3 )"></ion-input>
+                        <ion-input v-if="!rhoShow" v-model="rho" type="number" placeholder="Enter Fluid Density ( kg/m^3 )"></ion-input>
+                        <ion-input v-if="rhoShow" :value="rhoShow" readonly></ion-input>
                     </ion-item>
                 </ion-list>
                 <br>
                 <ion-item>
                         <ion-label v-if="selection == 'h'">h<sub>L</sub>&nbsp;&nbsp; : </ion-label>
                         <ion-label v-if="selection == 'p'">P<sub>L</sub>&nbsp;&nbsp; : </ion-label>
+
+                        <ion-input v-model="hl" v-if="selection == 'h' && !hShow" type="number" placeholder="Enter Head Loss ( m )"></ion-input>
+                        <ion-input v-model="pl" v-if="selection == 'p' && !pShow" type="number" placeholder="Enter Pressure Drop ( kPa )"></ion-input>
+
                         <ion-input v-if="hShow" :value=hShow readonly></ion-input>
                         <ion-input v-if="pShow" :value=pShow readonly></ion-input>
                 </ion-item>
@@ -94,7 +107,9 @@ export default defineComponent({
             d: "",
             rho: "",
             l: "",
-            f: ""
+            f: "",
+            hl: "",
+            pl: ""
         }
     },
     computed: {
@@ -107,7 +122,70 @@ export default defineComponent({
         },
         pShow() {
             if (this.vave != "" && this.d != "" && this.l != "" && this.f != "" && this.rho != "" && this.selection == "p") {
-                return String(parseFloat(0.001*this.f*this.l*this.vave**2*this.rho/(this.d*2)).toFixed(this.$store.state.decimal))+" kPa"
+                return parseFloat(0.001*this.f*this.l*this.vave**2*this.rho/(this.d*2)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        v1Show() {
+            if (this.hl != "" && this.d != "" && this.l != "" && this.f != "" && this.selection == "h") {
+                return parseFloat(Math.sqrt(this.hl*this.d*2*9.81/(this.l*this.f))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        v2Show() {
+            if (this.pl != "" && this.d != "" && this.l != "" && this.f != "" && this.rho != "" && this.selection == "p") {
+                return parseFloat(Math.sqrt(2000*this.pl*this.d/(this.rho*this.f*this.l))).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        f1Show() {
+            if (this.hl != "" && this.d != "" && this.l != "" && this.vave != "" && this.selection == "h") {
+                return parseFloat((2*this.hl*this.d*9.81)/(this.l*this.vave**2)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        f2Show() {
+            if (this.pl != "" && this.d != "" && this.l != "" && this.vave != "" && this.rho != "" && this.selection == "p") {
+                return parseFloat((2000*this.d*this.pl/this.rho)/(this.l*this.vave**2)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        l1Show() {
+            if (this.hl != "" && this.d != "" && this.f != "" && this.vave != "" && this.selection == "h") {
+                return parseFloat(this.hl*this.d*2*9.81/(this.f*this.vave**2)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        l2Show() {
+            if (this.pl != "" && this.d != "" && this.f != "" && this.vave != "" && this.rho != "" && this.selection == "p") {
+                return parseFloat((2000*this.pl*this.d/this.rho)/(this.f*this.vave**2)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        d1Show() {
+            if (this.hl != "" && this.l != "" && this.f != "" && this.vave != "" && this.selection == "h") {
+                return parseFloat(this.f*this.l*this.vave**2/(this.hl*2*9.81)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        d2Show() {
+            if (this.pl != "" && this.l != "" && this.f != "" && this.vave != "" && this.rho != "" && this.selection == "p") {
+                return parseFloat(this.f*this.l*this.rho*this.vave**2/(this.pl*2000)).toFixed(this.$store.state.decimal)
+            } else {
+                return ""
+            }
+        },
+        rhoShow() {
+            if (this.pl != "" && this.l != "" && this.f != "" && this.vave != "" && this.d != "" && this.selection == "p") {
+                return parseFloat(2000*this.pl*this.d/(this.f*this.l*this.vave**2)).toFixed(this.$store.state.decimal)
             } else {
                 return ""
             }
@@ -115,7 +193,6 @@ export default defineComponent({
     },
     methods: {
         clear() {
-            this.selection = "h",
             this.vave = "",
             this.d = "",
             this.rho = "",
@@ -125,18 +202,7 @@ export default defineComponent({
     },
     watch: {
         selection: function() {
-            if (this.selection == "h") {
-                this.vave = "",
-                this.d = "",
-                this.l = "",
-                this.f = ""
-            } else {
-                this.vave = "",
-                this.d = "",
-                this.rho = "",
-                this.l = "",
-                this.f = ""
-            }
+            this.clear()
         }
     }
 })
