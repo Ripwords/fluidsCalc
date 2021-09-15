f28d42029ee2c97a176da793942ca1b7ec61540a
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-09-15 23:58:15 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	publicPath: '/',
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/fluidsCalc/'
+    : '/',
 	configureWebpack: {
 		devServer: {
 			historyApiFallback: true,
