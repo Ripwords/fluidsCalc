8b4dd59520486e1b3b608c1006c35c47c496e861
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-09-19 21:03:35 +0800

    deploy

---
+++ precache-manifest.d498d429dc7633343ceb824af444d6cf.js
@@ -0,0 +1,498 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ade80494ab806035eae1fb5b7766b0a",
+    "url": "/fluidsCalc/assets/icon/favicon.png"
+  },
+  {
+    "revision": "0ade80494ab806035eae1fb5b7766b0a",
+    "url": "/fluidsCalc/assets/icon/icon.png"
+  },
+  {
+    "revision": "721967fb901ccf0910b3cdf7b77ba078",
+    "url": "/fluidsCalc/assets/shapes.svg"
+  },
+  {
+    "revision": "47459d4e2014780a2f6d",
+    "url": "/fluidsCalc/css/app.92ce97d2.css"
+  },
+  {
+    "revision": "5f931fbbf9b0505e2130",
+    "url": "/fluidsCalc/css/chunk-0ea5e900.9b5177f9.css"
+  },
+  {
+    "revision": "6e53831c9b6703432892",
+    "url": "/fluidsCalc/css/chunk-13383b6a.8c70cc41.css"
+  },
+  {
+    "revision": "357cf60c9a9cfb444549",
+    "url": "/fluidsCalc/css/chunk-134bc8ff.c1344ba2.css"
+  },
+  {
+    "revision": "e5d5778fa43bf0049481",
+    "url": "/fluidsCalc/css/chunk-144600f8.d52a4793.css"
+  },
+  {
+    "revision": "771026ecbc38f148f7e6",
+    "url": "/fluidsCalc/css/chunk-1fcbd31d.fa62201a.css"
+  },
+  {
+    "revision": "f133e043d6e305ca4954",
+    "url": "/fluidsCalc/css/chunk-2649ba26.de6c9987.css"
+  },
+  {
+    "revision": "da8e35d663f78e57872e",
+    "url": "/fluidsCalc/css/chunk-2ddc15dc.c1344ba2.css"
+  },
+  {
+    "revision": "9746877f0f10a1224863",
+    "url": "/fluidsCalc/css/chunk-2f101ad4.5266981b.css"
+  },
+  {
+    "revision": "7fcef374c55ce1dd03f1",
+    "url": "/fluidsCalc/css/chunk-354d5c58.86b703bc.css"
+  },
+  {
+    "revision": "26eb2761dc4f65a68b4b",
+    "url": "/fluidsCalc/css/chunk-3971aad1.9879b600.css"
+  },
+  {
+    "revision": "884c15bc4af119c24f22",
+    "url": "/fluidsCalc/css/chunk-3d777238.a04e33b0.css"
+  },
+  {
+    "revision": "c68c361229964b7b3a44",
+    "url": "/fluidsCalc/css/chunk-41ef49b9.b77b95e2.css"
+  },
+  {
+    "revision": "9c438055a76b680bb47f",
+    "url": "/fluidsCalc/css/chunk-43226d4f.f5dbedff.css"
+  },
+  {
+    "revision": "7929b8da7255daed1436",
+    "url": "/fluidsCalc/css/chunk-5477aa64.6bc2c4c6.css"
+  },
+  {
+    "revision": "c2737522d1a6394381f2",
+    "url": "/fluidsCalc/css/chunk-5c7ceef6.32588fed.css"
+  },
+  {
+    "revision": "c56a66cdb87c7729156a",
+    "url": "/fluidsCalc/css/chunk-5de1bdf1.78849993.css"
+  },
+  {
+    "revision": "69ca8679d988a91168f5",
+    "url": "/fluidsCalc/css/chunk-90565b54.04bd0ef6.css"
+  },
+  {
+    "revision": "5f39267307b431db3c38",
+    "url": "/fluidsCalc/css/chunk-98308526.6c1677cb.css"
+  },
+  {
+    "revision": "d6a29a4b8d750f2cf8f5",
+    "url": "/fluidsCalc/css/chunk-b2c6c3e0.9b9d3d60.css"
+  },
+  {
+    "revision": "679844f8a63df536fc6d",
+    "url": "/fluidsCalc/css/chunk-df5a0f98.c1344ba2.css"
+  },
+  {
+    "revision": "2092688e2fe62639684f",
+    "url": "/fluidsCalc/css/chunk-vendors.d0ded9bd.css"
+  },
+  {
+    "revision": "fd4af5cf30bf262b61f2a916301a818c",
+    "url": "/fluidsCalc/img/Vave.png"
+  },
+  {
+    "revision": "dc08ae5e9e45cb52b49b54c8344407e1",
+    "url": "/fluidsCalc/img/bEnergy.png"
+  },
+  {
+    "revision": "9d51c5b563f3693faceb6d49805652fe",
+    "url": "/fluidsCalc/img/bHead.png"
+  },
+  {
+    "revision": "aad2b4d55a46cab9ffe8cc75f66afeb8",
+    "url": "/fluidsCalc/img/bPressure.png"
+  },
+  {
+    "revision": "1804df3fcb3f725a82d0cf2626f7c2a9",
+    "url": "/fluidsCalc/img/cRise.png"
+  },
+  {
+    "revision": "b100667b4069c8ec5120faec2fd81b7f",
+    "url": "/fluidsCalc/img/density.png"
+  },
+  {
+    "revision": "4b992ac9e15711dadcd9c70b5a19327a",
+    "url": "/fluidsCalc/img/egl.png"
+  },
+  {
+    "revision": "bd884624b4c2b3a434d39e3887386b39",
+    "url": "/fluidsCalc/img/energyEqn.png"
+  },
+  {
+    "revision": "66c7f9a0be582a01d1ccb422516c7e8a",
+    "url": "/fluidsCalc/img/energyHeadEqn.png"
+  },
+  {
+    "revision": "e20edccb414543868ae9472bb4050b86",
+    "url": "/fluidsCalc/img/equation.png"
+  },
+  {
+    "revision": "d98211788d851cb14714f707d76c88cd",
+    "url": "/fluidsCalc/img/flowRate.png"
+  },
+  {
+    "revision": "3b1446fb512a075ed6c593551b6f0488",
+    "url": "/fluidsCalc/img/haaland.png"
+  },
+  {
+    "revision": "12f70fca52a3a65e9c3e0423070527b0",
+    "url": "/fluidsCalc/img/headLoss.png"
+  },
+  {
+    "revision": "9b605b486ed3213c7a2e81de44088d71",
+    "url": "/fluidsCalc/img/hgl.png"
+  },
+  {
+    "revision": "bb2a3670c93fd2be4e793ac882f1c13b",
+    "url": "/fluidsCalc/img/lamFriction.png"
+  },
+  {
+    "revision": "5f942ed0e0dcebad2dad9c6e4ba4f5f2",
+    "url": "/fluidsCalc/img/momentum.png"
+  },
+  {
+    "revision": "939402898b260e4ec5c606e63b648159",
+    "url": "/fluidsCalc/img/reynolds.png"
+  },
+  {
+    "revision": "9094eb687406edaabde43096e80193a7",
+    "url": "/fluidsCalc/img/stagnation.png"
+  },
+  {
+    "revision": "62bd88133bf10aa36d009694909c4fc0",
+    "url": "/fluidsCalc/img/turbFriction.png"
+  },
+  {
+    "revision": "7525d20ba37bab4ca376629ef118b52e",
+    "url": "/fluidsCalc/index.html"
+  },
+  {
+    "revision": "47459d4e2014780a2f6d",
+    "url": "/fluidsCalc/js/app.18d332d0.js"
+  },
+  {
+    "revision": "916995a542a182f113d9",
+    "url": "/fluidsCalc/js/chunk-02c995b5.c8dee7ab.js"
+  },
+  {
+    "revision": "a9f1696f7e067480bd7a",
+    "url": "/fluidsCalc/js/chunk-09155df3.e3ac211b.js"
+  },
+  {
+    "revision": "c33ac1675ee0c3d9ba3e",
+    "url": "/fluidsCalc/js/chunk-09162720.102e7a40.js"
+  },
+  {
+    "revision": "5359627315d5a230071d",
+    "url": "/fluidsCalc/js/chunk-097e0872.e6267a42.js"
+  },
+  {
+    "revision": "d38a23c0e380dbc2c768",
+    "url": "/fluidsCalc/js/chunk-0e93a01c.b0a5975b.js"
+  },
+  {
+    "revision": "5f931fbbf9b0505e2130",
+    "url": "/fluidsCalc/js/chunk-0ea5e900.14d6d955.js"
+  },
+  {
+    "revision": "6ddcbd4ed9b0f1c20181",
+    "url": "/fluidsCalc/js/chunk-106fab1a.186e0c8e.js"
+  },
+  {
+    "revision": "6e53831c9b6703432892",
+    "url": "/fluidsCalc/js/chunk-13383b6a.3efc5dd3.js"
+  },
+  {
+    "revision": "357cf60c9a9cfb444549",
+    "url": "/fluidsCalc/js/chunk-134bc8ff.dd7233b0.js"
+  },
+  {
+    "revision": "e5d5778fa43bf0049481",
+    "url": "/fluidsCalc/js/chunk-144600f8.0660aa29.js"
+  },
+  {
+    "revision": "b3422c9d57969a707c1b",
+    "url": "/fluidsCalc/js/chunk-1bff95de.bfdfffb4.js"
+  },
+  {
+    "revision": "fbaa99682929487be06c",
+    "url": "/fluidsCalc/js/chunk-1dbc15a2.8c41fb29.js"
+  },
+  {
+    "revision": "771026ecbc38f148f7e6",
+    "url": "/fluidsCalc/js/chunk-1fcbd31d.1300db51.js"
+  },
+  {
+    "revision": "10d1c35e3589a85b0a8a",
+    "url": "/fluidsCalc/js/chunk-21a76241.d966177c.js"
+  },
+  {
+    "revision": "6edc28fa3b4d1d8972ab",
+    "url": "/fluidsCalc/js/chunk-25d83ca8.6587c6be.js"
+  },
+  {
+    "revision": "f133e043d6e305ca4954",
+    "url": "/fluidsCalc/js/chunk-2649ba26.6b57f6c8.js"
+  },
+  {
+    "revision": "b5b704c84301503db112",
+    "url": "/fluidsCalc/js/chunk-2d0a463b.580f9170.js"
+  },
+  {
+    "revision": "21ba15a255a6a0478cc7",
+    "url": "/fluidsCalc/js/chunk-2d0b33e3.2e69defc.js"
+  },
+  {
+    "revision": "f0ac8b87c09e6e4ad1f2",
+    "url": "/fluidsCalc/js/chunk-2d0b9074.cf6d8bbf.js"
+  },
+  {
+    "revision": "d8ea207e3a9fae89217f",
+    "url": "/fluidsCalc/js/chunk-2d0b9280.58416f48.js"
+  },
+  {
+    "revision": "564f0792c621c5c04375",
+    "url": "/fluidsCalc/js/chunk-2d0baac9.ecbd8a2b.js"
+  },
+  {
+    "revision": "f78fd3b2e51a22baae47",
+    "url": "/fluidsCalc/js/chunk-2d0c073f.017f6960.js"
+  },
+  {
+    "revision": "0ad07f107421f9ab12fe",
+    "url": "/fluidsCalc/js/chunk-2d0d43da.c6672c38.js"
+  },
+  {
+    "revision": "2dccdfe939821adb706d",
+    "url": "/fluidsCalc/js/chunk-2d0da04a.620f425b.js"
+  },
+  {
+    "revision": "68f95d1cd8de1df8b43f",
+    "url": "/fluidsCalc/js/chunk-2d0e5812.730e6176.js"
+  },
+  {
+    "revision": "88259f7400b89a734e58",
+    "url": "/fluidsCalc/js/chunk-2d2073d6.9e36930f.js"
+  },
+  {
+    "revision": "32b0b3dcf853ab99f765",
+    "url": "/fluidsCalc/js/chunk-2d213189.2b11f265.js"
+  },
+  {
+    "revision": "43001d3f48887291426f",
+    "url": "/fluidsCalc/js/chunk-2d217e6a.9d5e5387.js"
+  },
+  {
+    "revision": "dfe50525ff958069dde9",
+    "url": "/fluidsCalc/js/chunk-2d218068.115cead1.js"
+  },
+  {
+    "revision": "fc7b2f95d49c48c8414a",
+    "url": "/fluidsCalc/js/chunk-2d21da73.56e20878.js"
+  },
+  {
+    "revision": "a49fbb56d12037370dd1",
+    "url": "/fluidsCalc/js/chunk-2d2297f7.78980670.js"
+  },
+  {
+    "revision": "49380faa8efc50a7cd65",
+    "url": "/fluidsCalc/js/chunk-2d2304d2.72233652.js"
+  },
+  {
+    "revision": "fe1929d6f0985f65c532",
+    "url": "/fluidsCalc/js/chunk-2d2376e6.a05b70ee.js"
+  },
+  {
+    "revision": "08c0c20d063c3f8bae82",
+    "url": "/fluidsCalc/js/chunk-2d237b00.037b5772.js"
+  },
+  {
+    "revision": "da8e35d663f78e57872e",
+    "url": "/fluidsCalc/js/chunk-2ddc15dc.bad474f0.js"
+  },
+  {
+    "revision": "9746877f0f10a1224863",
+    "url": "/fluidsCalc/js/chunk-2f101ad4.65450745.js"
+  },
+  {
+    "revision": "2e86d9d39beb6b73dbec",
+    "url": "/fluidsCalc/js/chunk-30dd9750.6b7d6eb0.js"
+  },
+  {
+    "revision": "7fcef374c55ce1dd03f1",
+    "url": "/fluidsCalc/js/chunk-354d5c58.6ad3b784.js"
+  },
+  {
+    "revision": "575d4429d28913dbfee0",
+    "url": "/fluidsCalc/js/chunk-37f2300e.cc3c45b7.js"
+  },
+  {
+    "revision": "26eb2761dc4f65a68b4b",
+    "url": "/fluidsCalc/js/chunk-3971aad1.5345bfc1.js"
+  },
+  {
+    "revision": "884c15bc4af119c24f22",
+    "url": "/fluidsCalc/js/chunk-3d777238.8710b482.js"
+  },
+  {
+    "revision": "b2ba2273c82af3241226",
+    "url": "/fluidsCalc/js/chunk-3ed29de3.9e68e146.js"
+  },
+  {
+    "revision": "efb4907469ba6d4e25b3",
+    "url": "/fluidsCalc/js/chunk-41c0f7a4.5f0941d1.js"
+  },
+  {
+    "revision": "c68c361229964b7b3a44",
+    "url": "/fluidsCalc/js/chunk-41ef49b9.a7458ed1.js"
+  },
+  {
+    "revision": "9c438055a76b680bb47f",
+    "url": "/fluidsCalc/js/chunk-43226d4f.8189581a.js"
+  },
+  {
+    "revision": "cbeb78232b715283b9f6",
+    "url": "/fluidsCalc/js/chunk-462c869e.ae6dddd3.js"
+  },
+  {
+    "revision": "b303ebea99c66d69f54e",
+    "url": "/fluidsCalc/js/chunk-47245a33.b9e6a322.js"
+  },
+  {
+    "revision": "ecae3832ac35afa74925",
+    "url": "/fluidsCalc/js/chunk-4739acd0.18f7dcc3.js"
+  },
+  {
+    "revision": "d3b5e87f05a94c6546bb",
+    "url": "/fluidsCalc/js/chunk-485adc83.ad48a8bb.js"
+  },
+  {
+    "revision": "9037451eab0359edc00c",
+    "url": "/fluidsCalc/js/chunk-53b1137a.240aa487.js"
+  },
+  {
+    "revision": "ccfba1ffdefbac2b9f84",
+    "url": "/fluidsCalc/js/chunk-541ea42d.63c37d24.js"
+  },
+  {
+    "revision": "7929b8da7255daed1436",
+    "url": "/fluidsCalc/js/chunk-5477aa64.c49c09c0.js"
+  },
+  {
+    "revision": "e254958ac2fbfd0542c3",
+    "url": "/fluidsCalc/js/chunk-54bbd082.dbfcec05.js"
+  },
+  {
+    "revision": "c5a12c05e4fcbf8ecc05",
+    "url": "/fluidsCalc/js/chunk-59d4c87c.9a2960ba.js"
+  },
+  {
+    "revision": "6cb53b42ddf6265010de",
+    "url": "/fluidsCalc/js/chunk-5a8ac886.5d25722a.js"
+  },
+  {
+    "revision": "c2737522d1a6394381f2",
+    "url": "/fluidsCalc/js/chunk-5c7ceef6.addaf4c7.js"
+  },
+  {
+    "revision": "c56a66cdb87c7729156a",
+    "url": "/fluidsCalc/js/chunk-5de1bdf1.e9523d5e.js"
+  },
+  {
+    "revision": "f386a2ec6c8748dffae3",
+    "url": "/fluidsCalc/js/chunk-5ff96a24.d3b261e5.js"
+  },
+  {
+    "revision": "12b7f5aee81eaf48584e",
+    "url": "/fluidsCalc/js/chunk-69eb8776.cb9d9263.js"
+  },
+  {
+    "revision": "f215811c4c408e27f968",
+    "url": "/fluidsCalc/js/chunk-6d375f34.8b250ad4.js"
+  },
+  {
+    "revision": "ae96b6fd8c0e77b17124",
+    "url": "/fluidsCalc/js/chunk-7548ca2d.18f11e7a.js"
+  },
+  {
+    "revision": "3901bb5ac6a64486c6c8",
+    "url": "/fluidsCalc/js/chunk-79f1dac7.4481f814.js"
+  },
+  {
+    "revision": "3d61bda538780760ae08",
+    "url": "/fluidsCalc/js/chunk-858cd918.54b9acef.js"
+  },
+  {
+    "revision": "c2157c9e7c7c822ee62b",
+    "url": "/fluidsCalc/js/chunk-8bb7f81e.4465b78a.js"
+  },
+  {
+    "revision": "69ca8679d988a91168f5",
+    "url": "/fluidsCalc/js/chunk-90565b54.e7f5f653.js"
+  },
+  {
+    "revision": "5f39267307b431db3c38",
+    "url": "/fluidsCalc/js/chunk-98308526.5ff29419.js"
+  },
+  {
+    "revision": "2f459fbd3853a20e231b",
+    "url": "/fluidsCalc/js/chunk-a9395c36.8777d2b5.js"
+  },
+  {
+    "revision": "d6a29a4b8d750f2cf8f5",
+    "url": "/fluidsCalc/js/chunk-b2c6c3e0.9ed461e2.js"
+  },
+  {
+    "revision": "897d09f34c28c7e7ca5d",
+    "url": "/fluidsCalc/js/chunk-d0e8aa0a.ede029b8.js"
+  },
+  {
+    "revision": "1d26fdbb12ec677ec08b",
+    "url": "/fluidsCalc/js/chunk-d8413eac.e1c32b8b.js"
+  },
+  {
+    "revision": "679844f8a63df536fc6d",
+    "url": "/fluidsCalc/js/chunk-df5a0f98.088bbcb2.js"
+  },
+  {
+    "revision": "ebcfbe9d137e7c95bd9c",
+    "url": "/fluidsCalc/js/chunk-e5275ddc.fb4bde6d.js"
+  },
+  {
+    "revision": "b7c2dffece48262fe4e8",
+    "url": "/fluidsCalc/js/chunk-eaa2b130.700c0fb3.js"
+  },
+  {
+    "revision": "2092688e2fe62639684f",
+    "url": "/fluidsCalc/js/chunk-vendors.b65d08d9.js"
+  },
+  {
+    "revision": "c6ef1925cf7e7731df86",
+    "url": "/fluidsCalc/js/polyfills-core-js.12be3461.js"
+  },
+  {
+    "revision": "f8a0775bfceb8f823175",
+    "url": "/fluidsCalc/js/polyfills-dom.662760da.js"
+  },
+  {
+    "revision": "ae8c5d20408de8f079a6e8a0349413e4",
+    "url": "/fluidsCalc/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/fluidsCalc/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fluidsCalc/precache-manifest.d498d429dc7633343ceb824af444d6cf.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "fluids_calc"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
